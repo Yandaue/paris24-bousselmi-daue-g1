75b311a686ca27e46e25b3a2c03b1e0967d8eb5e
Louloubrux <166617024+Louloubrux@users.noreply.github.com>
2024-04-10 14:51:04 +0200

    Update wordcloud.py

--- wordcloud.py
+++ wordcloud.py
@@ -36,3 +36,18 @@ plt.show()
 # exercise 3
 # author: BL
 # state: ongoing
+# Exercice 3--------------------------------------------------------------------
+
+# White font and excluding the word THY
+canvas_width=1920
+canvas_height=1080
+wordcloud = WordCloud(width=canvas_width,height=canvas_height).generate(text)
+stopwords = set(STOPWORDS)
+stopwords.add("thy")
+wordcloud = WordCloud(stopwords=stopwords,background_color='white',random_state=1,colormap='hot',max_font_size=800,min_font_size=20,width=canvas_width,height=canvas_height).generate(text)
+wordcloud.to_file("simple_wordcloud.png")
+plt.figure(figsize = (10, 10), facecolor=None)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.tight_layout(pad = 0)
+plt.show()
